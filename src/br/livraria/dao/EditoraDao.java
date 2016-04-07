192b6298b2a4d541b424ebf2d18de8416fa15ecc
Beatriz <beatriz_mqueiroz@yahoo.com.br>
2016-04-07 15:49:25 -0300

    Criei a classe Editora no pacote br.livraria.dominio Criei as classes EditoraDao e EditoraDaoImpl no pacote br.livraria.dao

---
+++ src/br/livraria/dao/EditoraDao.java
@@ -0,0 +1,16 @@
+package br.livraria.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.livraria.dominio.Editora;
+
+
+public interface EditoraDao {
+	
+	public void adicionarEditora(Editora ed) throws SQLException;
+	public void editarEditora(Editora ed) throws SQLException;
+	public void excluirEditora(Editora ed) throws SQLException;
+	public List<Editora> listaEditoras() throws SQLException;
+	
+}
