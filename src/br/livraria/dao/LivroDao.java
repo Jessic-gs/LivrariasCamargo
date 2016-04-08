4851a0a4c0474b4b00da6c4483441e3002a69d3c
hrick <henrique963@hotmail.com>
2016-04-08 12:22:13 -0300

    Terminando o html do inserir livro e criei as classes dao

---
+++ src/br/livraria/dao/LivroDao.java
@@ -0,0 +1,13 @@
+package br.livraria.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.livraria.dominio.Livro;
+
+public interface LivroDao {
+	
+	public void adicionar(Livro l) throws SQLException;
+	public List<Livro> pesquisarPorTodos() throws SQLException;
+	public void remover(Livro l) throws SQLException;
+}
---
+++ src/br/livraria/dao/LivroDaoImpl.java
@@ -0,0 +1,28 @@
+package br.livraria.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import br.livraria.dominio.Livro;
+
+public class LivroDaoImpl implements LivroDao{
+
+	@Override
+	public void adicionar(Livro l) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Livro> pesquisarPorTodos() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remover(Livro l) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
