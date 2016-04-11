2689a4f3d39da2ec7490cefd27d5b7ecb5e72b24
hrick <henrique963@hotmail.com>
2016-04-11 10:12:44 -0300

    Coloquei as Annotations na Editora

--- src/br/livraria/dominio/Editora.java
+++ src/br/livraria/dominio/Editora.java
@@ -1,37 +1,50 @@
 package br.livraria.dominio;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
 public class Editora {
-	
+
 	private int cnpj;
 	private String nome;
 	private String endereco;
 	private String telefone;
-		
+
+	@Id
+	@GeneratedValue
 	public int getCnpj() {
 		return cnpj;
 	}
+	@Column
 	public String getNome() {
 		return nome;
 	}
-	public String getEndereco(){
+	@Column
+	public String getEndereco() {
 		return endereco;
 	}
-	public String getTelefone(){
+	@Column
+	public String getTelefone() {
 		return telefone;
 	}
 
 	public void setCnpj(int cnpj) {
 		this.cnpj = cnpj;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public void setEndereco(String endereco){
+
+	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
-	public void setTelefone(String telefone){
+
+	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-		
+
 }
